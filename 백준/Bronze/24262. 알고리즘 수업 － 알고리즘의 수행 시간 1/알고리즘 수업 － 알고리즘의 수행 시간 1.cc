90e7f1b91a7a76e8ae155a5418c6e743af109c2c
yhmun1991 <yhmun1991@gmail.com>
2025-07-29 18:39:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int input_data = 0,  result = 0;
+
+    cin >> input_data;
+
+    cout << 1<<endl;
+    cout << 0;
+    return 0;
+}
