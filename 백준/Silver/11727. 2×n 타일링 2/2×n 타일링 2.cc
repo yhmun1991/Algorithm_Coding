fba98987a0dfe74d0cf2aded500341a89f846f08
yhmun1991 <yhmun1991@gmail.com>
2025-12-15 17:55:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <cmath>
+#include <set>
+#include <string>
+#include <cctype>
+#include <unordered_map>
+#include <unordered_set>
+#include <numeric>
+#include <list>
+
+
+#define CHAR_OFFSET 48
+#define VALIDFAIL -1
+using namespace std;
+
+
+class InputBase {//입력 클래스 생성
+public:
+    template<typename T>
+    bool Input(int count, vector<T>& inputData, int validOption, int min, int max) {
+        for (int i = 0; i < count; ++i) {
+            T data;
+
+            cin >> data;
+
+            if (!validCheck(validOption, data, min, max))
+                return false;
+
+            inputData.push_back(data);
+        }
+        return true;
+    }
+
+    bool validCheck(int aoption, int adata, int amin, int amax) {
+        bool ret = false;
+        switch (aoption) {
+        case 0:
+            if (adata < amin || adata > amax)
+                ret = false;
+            else
+                ret = true;
+            break;
+        default:
+            ret = true;
+        }
+
+        return ret;
+    }
+    bool validCheck(int aoption, long long adata, long long amin, long long amax) {
+        bool ret = false;
+        switch (aoption) {
+        case 0:
+            if (adata < amin || adata > amax)
+                ret = false;
+            else
+                ret = true;
+            break;
+        default:
+            ret = true;
+        }
+
+        return ret;
+    }
+
+    bool validCheck(int aoption, const string& adata, int amin, int amax) {
+        switch (aoption) {
+        case 0:
+            return true;
+        default:
+            return true;
+        }
+    }
+};
+
+class process {//처리 클래스
+public:
+    void run(vector<string>& aindata, vector<string>& outdata)
+    {
+        vector<int> vec_data, vec_distance;
+        int origin, first, second, count, result, tmp_data;
+        outdata.push_back("");
+        list<string> lst(aindata.begin(), aindata.end());
+        findTarget(lst, outdata, "KBS1", 0);
+        findTarget(lst, outdata, "KBS2", 1);
+        
+    }
+
+    void findTarget(list<string>& aindata, vector<string>& outdata, string target, int option)
+    {
+        int index;
+        auto it = std::find(aindata.begin(), aindata.end(), target);
+
+        if (it != aindata.end()) {
+            index = std::distance(aindata.begin(), it);
+        }
+        for (int i = 0; i < index; i++)
+        {
+            outdata[0].push_back('1');
+        }
+        for (int i = 0; i < index-option; i++)
+        {
+            outdata[0].push_back('4');
+        }
+        
+        string value = *it;
+        aindata.erase(it);
+        aindata.push_front(value);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int inputData, result = 0, count = 0;
+    int a, b,c;
+
+    cin >> inputData;
+    result = inputData;
+    
+    if (inputData == 1) {
+        cout << 1;
+        return 0;
+    }
+    if (inputData == 2) {
+        cout << 3;
+        return 0;
+    }
+
+    a = 1; // dp[1]
+    b = 3; // dp[2]
+    for (int i = 3; i <= inputData; i++)
+    {
+        c = (b + 2 * a);
+        c = c % 10007;
+        a = b;
+        b = c;
+    }
+    cout << b;
+}
